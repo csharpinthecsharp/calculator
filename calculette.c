6369e9b7b33975e1911b48b23b9d68abba135f64
Calc editor <trilllucien@gmail.com>
2025-05-06 09:48:15 +0200

    Support for negative numbers

--- calculette.c
+++ calculette.c
@@ -31,6 +31,7 @@ void ft_putnbr(int nb)
 	if (nb < 0)
 	{
 		nb = -nb;
+		ft_putchar('-');
 	}
 	if (nb >= 10)
 	{
@@ -46,7 +47,7 @@ void ft_putnbr(int nb)
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	int temp = 0;
+	int sign = 1;
 	char operator; 
 	int x = 0;
 	int y = 0;
@@ -87,27 +88,48 @@ int main(int argc, char *argv[])
 			x = x * 10 + (argv[1][i] - '0');
 			i++;
 		}
+		else if (argv[1][i] == '-')
+		{
+			sign *= -1;
+			i++;
+		}
 		else
 		{
 			ft_putstr("Argument 1 seem to not be a number");
 			return (0);
 		}
 	}
+	x *= sign;
+	sign = 1;
 	i = 0;
 	/*Loop to get the second number*/
 	while (argv[3][i] != '\0')
 	{
 		if (argv[3][i] >= '0' && argv[3][i] <= '9')
 		{
-			y = y * 10 + (argv[1][i] - '0');
+			y = y * 10 + (argv[3][i] - '0');
 			i++;
+		}
+		else if (argv[3][0] == '-')
+		{
+			{
+				sign *= -1;
+				i++;
+			}
 		}
 		else
 		{
 			ft_putstr("Argument 3 seem to not be a number");
 		}
 	}
+	y *= sign;
 	i = 0;
+
+	if (x < 0 && y < 0)
+	{
+		x *= 1;
+		y *= 1;
+	}
 	if (operator == '*')
 	{
 		ft_putnbr(x * y);
