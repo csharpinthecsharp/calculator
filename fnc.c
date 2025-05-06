5112703fef8d67be7a13b6d8ba598cb98a59e85a
Calc editor <trilllucien@gmail.com>
2025-05-06 11:53:17 +0200

    Separate files

---
+++ fnc.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	char c;
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
--- main.c
+++ main.c
@@ -1,48 +1,4 @@
-#include <unistd.h>
-
-void ft_putstr(char *str)
-{
-	int i = 0;
-	while (str[i] != '\0')
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-}
-
-int ft_strlen(char *str)
-{
-	int i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void ft_putnbr(int nb)
-{
-	char c;
-	if (nb < 0)
-	{
-		nb = -nb;
-		ft_putchar('-');
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-	{
-		ft_putchar(nb + '0');
-	}
-}
+ #include "fnc.c"
 
 int main(int argc, char *argv[])
 {
